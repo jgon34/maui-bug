77d082e69640b8e7a6b55981fe00d769884cd198
Joshua Gonzalez <jjgonzalez@3-gis.com>
2024-05-16 10:51:55 -0400

    Maui Visual State Manager Bug

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        List<string> list = ["1", "2", "3", "4", "5"];
+        ResultList.ItemsSource = list;
+        ResultList.SelectedItem = null;
+    }
+
+    private void ResultList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private async void CounterBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new NewPage1());
+    }
+}
+
